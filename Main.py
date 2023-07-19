5ad0491bcb70e4f6ee41b60f2acc55ad711172c3
WingsOfAnEagle <griffin.mikhail@gmail.com>
2023-07-19 13:39:05 -0700

    Added a better menu. Also added a file to store the character_data in.

--- Main.py
+++ Main.py
@@ -1,12 +1,21 @@
 import sys
 
-
+menu_main = ['Create new character(1)', 'Saved characters(2)', 'Exit(3)']
 loop = True 
 
 while loop:
-    print('this is a sting')
-    exit = input('this is a stopping point')
-    if exit == '1': 
-        loop = False
+    print('\n'.join(menu_main))
 
-    
+    selection = int(input('\n\n\nEnter choice here: '))
+
+    match selection:
+        case 1:
+            print('hello')
+            break               
+        case 2:
+            print('hi')
+            break       
+        case 3:
+            print('hey')    
+            loop = False
+            break
